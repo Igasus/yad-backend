50770d66fa773762c3cbf2a9d053da66e290c375
Ihor Hubskyi <igasus@MacBook-Air-Ihor.local>
2020-04-14 02:41:46 +0300

    Added projects GET/POST requests

--- admin.js
+++ admin.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongodb = require('mongodb');
 const assert = require('assert');
+const session = require('express-session');
 
 const router = express.Router();
 
+router.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true
+}));
 
 const MongoClient = mongodb.MongoClient;
 const mongoUrl = 'mongodb+srv://Igasus:11062002Igasus@yad-database-cluster-r8sn3.gcp.mongodb.net/test?retryWrites=true&w=majority';
@@ -22,13 +28,16 @@ const mainAccount = {
 }
 const databaseReconnectDelay = 10000; //In miliseconds
 
-
 function connectDatabase() {
   mongoClient.connect(err => {
     if (err) {
       console.log("Error while connecting yad-database:");
       console.log(err);
       accounts = [mainAccount];
+      console.log("Next connection try in " + databaseReconnectDelay + " miliseconds.");
+      setTimeout(() => {
+        connectDatabase();
+      }, databaseReconnectDelay);
       return false;
     }
     db = mongoClient.db('yad-database');
@@ -42,14 +51,15 @@ function connectDatabase() {
   });
 }
 
+connectDatabase();
+
+
 
 
 let updateAccountsOnServer = async () => {
-  if (!db) {
-    connectDatabase();
-    return;
-  }
   accounts = [mainAccount];
+  if (!db)
+    return;
   let cursor = accountsCollection.find();
   await cursor.forEach((account) => accounts.push(account));
 }
@@ -62,7 +72,7 @@ function showAccounts() {
 
 
 
-function findAccountByUserame(username) {
+function findAccountByUsername(username) {
   let account = null;
   accounts.forEach((acc) => {
     if (acc.username == username)
@@ -73,23 +83,16 @@ function findAccountByUserame(username) {
 
 
 
-if (!connectDatabase()) {
-  setTimeout(()=> {
-
-  }, databaseReconnectDelay);
-};
-
-
 
 router.post('/login', (req, res) => {
-  res.redirect('http://localhost:4200/admin');
-  let account = findAccountByLogin(req.body.username);
+  console.log(req.body.username  + " " + req.body.password);
+  let account = findAccountByUsername(req.body.username);
   if (account) {
     if (req.body.password == account.password) {
       console.log('\n(POST):admin/login  ->  Log-in successed:');
       console.log(account);
-      res.json({ result: true });      // True - Log-in successed.
-      return;
+      req.session.isLoggedIn = true;
+      return res.send(true);      // True - Log-in successed.
     }
 
     console.log('\n(POST):admin/login  ->  Incorrect password:');
@@ -101,40 +104,50 @@ router.post('/login', (req, res) => {
     RequestUsername: req.body.username,
     RequestPassword: req.body.password
   });
-  res.json({ result: false });     // False - Incorrect data.
+  return res.send(false);     // False - Incorrect data.
 });
 
 
 
 router.post('/register', (req, res) => {
-  res.redirect('http://localhost:4200/admin');
 
-  let account = findAccountByLogin(req.body.login);
+  let account = findAccountByUsername(req.body.username);
   if (account) {
     console.log('\n(POST):admin/register  ->  Account already exists:');
     console.log(account);
-    res.json({ result: false });      // False - Account already exist.
-    return;
+    return res.send(false);      // False - Account already exist.
   }
 
   account = {
     username: req.body.username,
     password: req.body.password,
     post: 'worker'
-  }
+  };
+
   accountsCollection.insertOne(account)
   .then((result) => {
     updateAccountsOnServer();
     console.log('\n(POST):admin/register  ->  Account inserted to database:');
     console.log(account);
-    res.json({ result: true });     // True - Account created.
+    return res.send(true);     // True - Account created.
   });
 });
 
 
 
 router.get('/check-session', (req, res) => {
+  // if (req.session.isLoggedIn)
+  //   return res.send(true);
+  // else {
+  //   req.session.isLoggedIn = false;
+  //   return res.send(false);
+  // }
 
+  if (!req.session.counter)
+    req.session.counter = 0;
+  req.session.counter++;
+
+  return res.json({data: req.session.counter});
 });
 
 
--- index.js
+++ index.js
@@ -1,20 +1,274 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const admin = require('./admin');
-const gallery = require('./gallery');
 const cors = require('cors');
+const mongodb = require('mongodb');
+const assert = require('assert');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const multer = require('multer');
+const fs = require('fs');
 
 const app = express();
 const port = 5000;
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'projects/');
+  },
+  filename: (req, file, cb) => {
+    let uniquePrefix = 1000000;
+    let projectImages = getDirectoryFiles('./projects/');
+    if (projectImages.length > 0) {
+      let lastImage = projectImages[projectImages.length-1];
+      uniquePrefix = parseInt(lastImage.slice(0, 7)) + 1;
+    }
+    cb(null, uniquePrefix + '-' + file.originalname);
+  }
+});
+const upload = multer({ storage: multerStorage });
+
+
+let getDirectoryFiles = (path) => {
+  let directoryFiles = [];
+  let allDirectoryFiles = fs.readdirSync(path);
+  for (let fileName of allDirectoryFiles) {
+    if (fileName[0] == '.')
+      continue;
+    directoryFiles.push(fileName);
+  }
+  return directoryFiles;
+}
+
+
+
+const MongoClient = mongodb.MongoClient;
+const mongoUrl = 'mongodb+srv://Igasus:11062002Igasus@yad-database-cluster-r8sn3.gcp.mongodb.net/test?retryWrites=true&w=majority';
+const mongoClient = new MongoClient(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+
+const sessionsStore = new MongoDBStore({
+  uri: mongoUrl,
+  collection: 'Sessions'
+});
+
+sessionsStore.on('error', function(error) { console.log(error); });
+
+app.use(session({
+  secret: 'keyboard cat',
+  store: sessionsStore,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 }
+}));
 
 
 app.use(cors({
-  origin: 'http://localhost:4200/'
+  origin: 'http://localhost:4200',
+  credentials: true,
 }));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/admin', admin);
-app.use('/gallery', gallery);
+app.use('/projects', express.static(__dirname + '/projects'));
+
+
+
+
+let db = null;
+let accountsCollection = null;
+let accounts = [];
+const mainAccount = {
+  username: "admin",
+  password: "admin",
+  post: "admin"
+}
+const databaseReconnectDelay = 2000; //In miliseconds
+
+function connectDatabase() {
+  mongoClient.connect(err => {
+    if (err) {
+      reconnectPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          connectDatabase();
+        }, databaseReconnectDelay);
+      });
+
+      console.log("Error while connecting yad-database:");
+      console.log(err);
+      accounts = [mainAccount];
+      console.log("\n\nNext connection try in " + databaseReconnectDelay + " miliseconds.");
+      reconnectPromise;
+      return false;
+    }
+    db = mongoClient.db('yad-database');
+    accountsCollection = db.collection('Accounts');
+    console.log("\n\nyad-database has been connected successfuly!");
+    updateAccountsOnServer().then(() => {
+      console.log("\n\nyad-database accounts:");
+      console.log(accounts);
+    });
+    return true;
+  });
+}
+connectDatabase();
+
+
+let updateAccountsOnServer = async () => {
+  accounts = [mainAccount];
+  if (!db)
+    return;
+  let cursor = accountsCollection.find();
+  await cursor.forEach((account) => accounts.push(account));
+}
+
+
+let showAccounts = () => {
+  console.log(accounts);
+}
+
+
+let findAccountByUsername = (username) => {
+  let account = null;
+  accounts.forEach((acc) => {
+    if (acc.username == username)
+      account = acc;
+  });
+  return account;
+}
+
+
+
+
+app.post('/admin/sign/in', (req, res) => {
+  let account = findAccountByUsername(req.body.username);
+  if (account) {
+    if (req.body.password == account.password) {
+      console.log('\n\n(POST):admin/sign/in  ->  Log-in successed:');
+      console.log(account);
+      req.session.username = account.username;
+      return res.send(true);      // True - Log-in successed.
+    }
+
+    console.log('\n\n(POST):admin/sign/in  ->  Incorrect password:');
+  }
+  else
+    console.log('\n\n(POST):admin/sign/in  ->  Unexisted login:');
+
+  console.log({
+    RequestUsername: req.body.username,
+    RequestPassword: req.body.password
+  });
+  return res.send(false);     // False - Incorrect data.
+});
+
+
+
+
+app.post('/admin/sign/up', (req, res) => {
+  let account = findAccountByUsername(req.body.username);
+  if (account) {
+    console.log('\n\n(POST):admin/sign/up  ->  Account already exists:');
+    console.log(account);
+    return res.send(false);      // False - Account already exist.
+  }
+
+  account = {
+    username: req.body.username,
+    password: req.body.password,
+    post: 'worker'
+  };
+
+  accountsCollection.insertOne(account)
+  .then((result) => {
+    updateAccountsOnServer();
+    console.log('\n\n(POST):admin/sign/up  ->  Account inserted to database:');
+    console.log(account);
+    return res.send(true);     // True - Account created.
+  });
+});
+
+
+
+
+app.get('/admin/sign/out', (req, res) => {
+  console.log("\n\n(GET):admin/sign/out  ->  SessionID: " + req.sessionID + "\n  Cleared login: " + req.session.username);
+  req.session.username = "";
+  return res.send(true);
+});
+
+
+
+
+app.get('/admin/sign/check', (req, res) => {
+  if (req.session.username) {
+    console.log("\n\n(GET):admin/sign/check  ->  SessionID: " + req.sessionID + "\n  Result: True \n  Username: " + req.session.username);
+    return res.send(true);
+  }
+  // console.log("\n\n(GET):admin/sign/check  ->  SessionID: " + req.sessionID + "\n  Result: False");
+  return res.send(false);
+});
+
+
+
+
+app.get('/admin/account', (req, res) => {
+  let username = req.session.username;
+  let account = findAccountByUsername(username);
+  if (account) {
+    console.log("\n\n(GET):admin/account  ->  SessionID: " + req.sessionID + "\n  Account username: " + account.username);
+    return res.json(account);
+  }
+
+  return res.send(null);
+});
+
+
+
+
+app.post('/admin/account', (req, res) => {
+  let username = req.body.username;
+  console.log("\n\n(POST):admin/account  ->  Account username: " + req.session.username);
+
+  if (username) {
+    let account = findAccountByUsername(username);
+    console.log("Requested account " + username);
+    if (account) {
+      console.log("  Result: Success");
+      return res.json(account);
+    }
+
+    console.log("  Result: Not found");
+    return res.send(null);
+  }
+
+  console.log("Requested add accounts");
+  return res.json({accounts: accounts});
+});
+
+
+
+
+app.post('/projects/add', upload.single('file'), (req, res) => {
+  const file = req.file;
+  if (!file) {
+    console.log('\n\n(POST):projects/add  ->  Error: No file');
+    return res.send(false);
+  }
+  console.log('\n\n(POST):projects/add  ->  File has been added: ' + file.filename);
+  return res.send(true);
+});
+
+
+
+
+app.get('/projects/getLinks', (req, res) => {
+  let projects = getDirectoryFiles('./projects/');
+  res.send(projects);
+});
+
+
 
 
 app.listen(port, () => console.log('Backend is listening on port ' + port + '!'));
